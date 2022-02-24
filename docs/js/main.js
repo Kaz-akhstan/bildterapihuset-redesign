e3b376ce71fecc24a8d4dc645624bb9434babbb9
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-02-24 11:01:03 +0100

    Aktiverings knapp till sidbaren

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,3 +4,8 @@ function loadProfileFunc()
 {
     document.getElementById("profileBackground").style.zIndex ="1";
 }
+
+function sidebarActivationFunc()
+{
+    document.getElementById("sideBar").style.animation="contactBar:hover";
+}
