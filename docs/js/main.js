8358c3c59a037f4b79dfdd6f8f8c3b4d7d018d8b
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-03-03 11:04:33 +0100

    Saker

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,1 @@
 console.log("loaded");
-
-function loadProfileFunc()
-{
-    document.getElementById("profileBackground").style.zIndex ="1";
-}
-
-function sidebarActivationFunc()
-{
-    document.getElementById("sideBar").style.animation="contactBar:hover";
-}
