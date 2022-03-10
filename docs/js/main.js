a12f58c8c29353938d7d41388a2d80a8b508a5fd
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-03-10 03:51:45 +0100

    Omgjord startsida

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,21 @@
 console.log("loaded");
+
+function bokaTidFunc()
+{
+    window.location.href = "bokaNy.html"
+}
+
+function teraFunc()
+{
+    window.location.href = "tera.html"
+}
+
+function kontaktFunc()
+{
+    window.location.href = "kontakt.html"
+}
+
+function omOssFunc()
+{
+    window.location.href = "omOss.html"
+}
