d3bb26ac4f88213a88bbb772536c2907e8dc7a77
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-03-23 18:59:23 +0100

    Början på highlight

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,20 +2,28 @@ console.log("loaded");
 
 function bokaTidFunc()
 {
-    window.location.href = "bokaNy.html"
+    window.location.href = "bokaNy.html";
 }
 
 function teraFunc()
 {
-    window.location.href = "tera.html"
+    window.location.href = "tera.html";
 }
 
 function kontaktFunc()
 {
-    window.location.href = "kontakt.html"
+    window.location.href = "kontakt.html";
 }
 
 function omOssFunc()
 {
-    window.location.href = "omOss.html"
+    window.location.href = "omOss.html";
+}
+
+function highlightAnim()
+{
+    document.getElementById("greetingBokaID").style.boxShadow = "0px 0px 40px 10px white";
+    setTimeout(function(){
+        document.getElementById("greetingBokaID").style.boxShadow = "none";
+    }, 2000);
 }
